d069e4821f49ef4159aa84ae561779a5f7436579
Pankaj Kumar Gupta <140154353+pankajkrg@users.noreply.github.com>
2024-11-18 23:19:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "three" {
   count         =  3
-  ami           = "ami-00b8917ae86a424c9"
-  instance_type = "t2.medium"
-  key_name      = "mykeypair9am"
+  ami           = "ami-0aebec83a182ea7ea"
+  instance_type = "t2.small"
+  key_name      = "MY4pm"
   tags = {
     Name = "mumbai-server"
   }
 }
 
 resource "aws_s3_bucket" "abcd" {
-bucket = "devopsherahamshaik0099889977hcp"
+bucket = "devopsherahamshaik0099889977hcpankaj"
 }
